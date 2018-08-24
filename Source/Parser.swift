d09073cb9e6ec3e59d20d96025b301fb3aaa6f22
zero <zero.griffin@gmail.com>
2018-08-24 07:21:14 +0200

    Refactor parser

--- Source/Parser.swift
+++ Source/Parser.swift
@@ -34,25 +34,25 @@ public class Parser {
     }
     
     private func traits(string:String, stack:Stack) -> NSAttributedString {
-        let mutable:NSMutableAttributedString = NSMutableAttributedString()
-        let attributes:[NSAttributedString.Key:AnyObject] = [NSAttributedString.Key.font:stack.items.last!.font]
-        if let position:Position = self.next(string:string, stack:stack) {
+        let mutable = NSMutableAttributedString()
+        let font = stack.items.last!.font
+        if let position = next(string:string, stack:stack) {
             stack.add(interpreter:position.interpreter)
             mutable.append(NSAttributedString(string:String(string.prefix(upTo:position.index.lowerBound)),
-                                              attributes:attributes))
-            mutable.append(self.traits(string:String(string.suffix(from:position.index.upperBound)), stack:stack))
+                                              attributes:[.font:font]))
+            mutable.append(traits(string:String(string.suffix(from:position.index.upperBound)), stack:stack))
         } else {
-            mutable.append(NSAttributedString(string:string, attributes:attributes))
+            mutable.append(NSAttributedString(string:string, attributes:[.font:font]))
         }
         return mutable
     }
     
     private func next(string:String, stack:Stack) -> Position? {
         var character:Position?
-        self.traits.forEach { (item:Interpreter) in
+        traits.forEach { (item) in
             guard
                 stack.canBeNext(interpreter:item),
-                let interpreterIndex:Range<String.Index> = self.next(string:string, interpreter:item)
+                let interpreterIndex = next(string:string, interpreter:item)
             else { return }
             if character == nil || (character != nil && interpreterIndex.lowerBound < character!.index.lowerBound) {
                 character = Position(interpreter:item, index:interpreterIndex)
@@ -63,8 +63,8 @@ public class Parser {
     
     private func next(string:String, interpreter:Interpreter) -> Range<String.Index>? {
         var index:Range<String.Index>?
-        interpreter.match.forEach { (item:String) in
-            guard let range:Range<String.Index> = string.range(of:item) else { return }
+        interpreter.match.forEach { (item) in
+            guard let range = string.range(of:item) else { return }
             if index == nil || (index != nil && range.lowerBound < index!.lowerBound) {
                 index = range
             }
