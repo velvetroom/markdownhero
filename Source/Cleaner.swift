25ba91a0b9e58055a77fd4f94d85afcbdd635bce
zero <zero.griffin@gmail.com>
2018-08-24 07:38:26 +0200

    Refactor structs

--- Source/Cleaner.swift
+++ Source/Cleaner.swift
@@ -4,16 +4,16 @@ class Cleaner {
     private let items:[CleanerItem]
     
     init() {
-        self.items = [CleanerItem(match:"  ", replace:" "),
-                      CleanerItem(match:"\n\n\n", replace:"\n\n"),
-                      CleanerItem(match:"\n- ", replace:"\n• "),
-                      CleanerItem(match:"\n+ ", replace:"\n• "),
-                      CleanerItem(match:"\n* ", replace:"\n• ")]
+        items = [CleanerItem(match:"  ", replace:" "),
+                 CleanerItem(match:"\n\n\n", replace:"\n\n"),
+                 CleanerItem(match:"\n- ", replace:"\n• "),
+                 CleanerItem(match:"\n+ ", replace:"\n• "),
+                 CleanerItem(match:"\n* ", replace:"\n• ")]
     }
     
     func clean(string:String) -> String {
-        var cleaned:String = string
-        self.items.forEach { (item:CleanerItem) in
+        var cleaned = string
+        items.forEach { (item) in
             while cleaned.contains(item.match) {
                 cleaned = cleaned.replacingOccurrences(of:item.match, with:item.replace)
             }
